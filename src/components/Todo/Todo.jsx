ef0bea215c48899911d5d0bce0c2f6c389043e17
bakyerim <rumiobu03@gmail.com>
2023-11-22 14:23:11 +0900

    🚧 Todo List 반영되는 영역

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,23 +1,27 @@
 import React, {  } from 'react';
 import { FaTrashAlt } from 'react-icons/fa';
+import styles from './Todo.module.css'
 
 export default function Todo({todo, onUpdate, onDelete}) {
   // const [checked, setChecked] = useState(false);
-  const { text, status } = todo;
+  const { id, text, status } = todo;
   const handleChange = (e) => {
     const status = e.target.checked ? 'completed' : 'active';
     onUpdate({...todo, status})
   } 
   const handleDelete = () => onDelete(todo);
   return (
-  <li>
-    <input 
-    id='checkbox' 
+  <li className={styles.todo}>
+    <input
+    className={styles.checkbox}
+    id={id} 
     type='checkbox' 
     checked={status === 'completed'} 
     onChange={handleChange} />
-    <label htmlFor='checkbox'>{text}</label>
-    <button onClick={handleDelete}><FaTrashAlt /></button>
+    <label htmlFor={id} className={styles.text}>{text}</label>
+    <span className={styles.icon}>
+      <button onClick={handleDelete} className={styles.button}><FaTrashAlt /></button>
+    </span>
     </li>
   );
 }
